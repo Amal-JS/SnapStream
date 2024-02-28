741b4b3f381617410c5e18e5ee3e09ed5b2def77
amaljs <amaljs121@gmail.com>
2024-02-28 18:08:16 +0530

    added backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,8 @@ import { OtpVerification } from './Components/OtpVerification.tsx'
 import { UserEdit } from './Components/UserEdit.tsx'
 import { UserProfile } from './Components/UserProfile.tsx'
 import { UserStories } from './Components/UserStories.tsx'
+import { UserHome } from './Components/UserHome.tsx'
+
 function App() {
 
 
@@ -17,15 +19,17 @@ function App() {
   
   <BrowserRouter>
   <Routes>
-    <Route path='/' element={<UserProfile />} />
+    <Route path='/' element={<UserHome />} />
     <Route path='/signup' element={<Signup/>} />
     <Route path='/login' element={<Login/>}/>
     <Route path='/password/reset' element={<ForgotPassword/>}/>
     <Route path='/password/update/:id' element={<PasswordUpdate />} />
     <Route path='/password/update' element={<Navigate to="/404" />} />
     <Route path='/otpverify/:id' element={<OtpVerification/>} />
+    <Route path='/userprofile/:id' element={<UserProfile />} />
     <Route path='/viewstories/:id' element={<UserStories />}/>
     <Route path='/edituserprofile/:id' element={<UserEdit />}/>
+
 
     <Route path='*' element={<PageNotFound />}/>
 
---
+++ frontend/src/Components/UserHome.tsx
@@ -0,0 +1,7 @@
+export const UserHome = ()=> {
+    return (
+        <div className="p-3">
+            <h1 className="text-8xl text-black">Home</h1>
+        </div>
+    )
+}
