329ab21bfb218bc3be5d03bae8a1c63a5b054bc6
amaljs <amaljs121@gmail.com>
2024-03-13 12:02:48 +0530

    Tooltip added as a component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,13 +10,12 @@ import { UserEdit } from './Components/UserEdit.tsx'
 import { UserProfile } from './Components/UserProfile.tsx'
 import { UserStories } from './Components/UserStories.tsx'
 import { UserHome } from './Components/UserHome.tsx'
-import { MdSunny } from "react-icons/md";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { useEffect } from 'react'
-import { Button } from '@nextui-org/react'
 import { useAppSelector } from './hooks/redux.ts'
 import { useAppDispatch } from './hooks/redux.ts'
 import { themeToggle } from './Redux/authSlice.ts'
+import { ToolTip } from './Components/Tooltip/Tooltip.tsx'
 
 function App() {
 
@@ -32,25 +31,12 @@ function App() {
     }
   },[userState.darkTheme])
 
-  const handleThemeChange = ()=>{
-    dispatch(themeToggle())
-  }
+
 
 
   return (
   <>
-  <div className='absolute top-5 right-10 md:top-10 md:right-5   text-dark dark:text-white '>
-  <div className='relative'>
-  <MdSunny className=' text-4xl md:text-5xl hover:cursor-pointer has-tooltip border-0' data-tooltip-target="tooltip-default"  onClick={handleThemeChange}>Change Theme</MdSunny>
-  <div id="tooltip-default" role="tooltip" className=" tooltip absolute z-10  inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0  dark:bg-white dark:text-gray-900">
-    Change theme
-    <div className="tooltip-arrow" data-popper-arrow></div>
-</div>
-  </div>
-  
-  </div>
-  
-
+ <ToolTip/>
  <Toast />
  <GoogleOAuthProvider clientId={'443025233563-nh99nv2u8u7hj8l8o91bpguja6ni7114.apps.googleusercontent.com'}>
  
---
+++ frontend/src/Components/Tooltip/Tooltip.tsx
@@ -0,0 +1,27 @@
+import { MdSunny } from "react-icons/md"
+import { useAppDispatch } from "../../hooks/redux"
+import { themeToggle } from "../../Redux/authSlice"
+
+export const ToolTip = ()=>{
+
+    const dispatch=useAppDispatch()
+    
+    const handleThemeChange = ()=>{
+        dispatch(themeToggle())
+      }
+
+    return (
+        <div className='absolute top-5 right-10 md:top-10 md:right-5   text-dark dark:text-white '>
+        <div className='relative'>
+        <MdSunny className=' text-4xl md:text-5xl hover:cursor-pointer has-tooltip border-0' data-tooltip-target="tooltip-default"  onClick={handleThemeChange}>Change Theme</MdSunny>
+        <div id="tooltip-default" role="tooltip" className=" tooltip absolute z-10  inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0  dark:bg-white dark:text-gray-900">
+          Change theme
+          <div className="tooltip-arrow" data-popper-arrow></div>
+      </div>
+        </div>
+        
+        </div>
+        
+      
+    )
+}
